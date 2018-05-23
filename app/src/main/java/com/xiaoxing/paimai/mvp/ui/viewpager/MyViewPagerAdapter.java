8684613068e721276c6a096f294de31633ea5fb2
xiaoxing <753940262@qq.com>
2018-05-23 19:10:57 +0800

    1、SmartRefreshLayout框架添加 2、个人中心fragmentmine添加

--- app/src/main/java/com/xiaoxing/paimai/mvp/ui/viewpager/MyViewPagerAdapter.java
+++ app/src/main/java/com/xiaoxing/paimai/mvp/ui/viewpager/MyViewPagerAdapter.java
@@ -5,18 +5,20 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.xiaoxing.paimai.mvp.ui.fragment.FragmentMine;
+
 public class MyViewPagerAdapter extends FragmentPagerAdapter {
 
     private int size;
 
-    public MyViewPagerAdapter(FragmentManager fm,int size) {
+    public MyViewPagerAdapter(FragmentManager fm, int size) {
         super(fm);
         this.size = size;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return AFragment.newInstance(position + "");
+        return FragmentMine.newInstance(position + "");
     }
 
     @Override
