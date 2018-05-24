fcd189aabb5484751b36d948587afc6d19b39278
xiaoxing <753940262@qq.com>
2018-05-24 22:25:44 +0800

    1、首页搜索框添加

--- app/src/main/java/com/xiaoxing/paimai/mvp/ui/fragment/FragmentHome.java
+++ app/src/main/java/com/xiaoxing/paimai/mvp/ui/fragment/FragmentHome.java
@@ -3,7 +3,6 @@ package com.xiaoxing.paimai.mvp.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
