22b3e8b8a19f6c898efb8007d2c6adb124714b62
xiaoxing <753940262@qq.com>
2018-05-29 17:41:20 +0800

    1、验证码页面

--- XiaoxingLogin/src/main/java/com/xiaoxing/login/mvp/ui/activity/RegisterVerificationCodeActivity.java
+++ XiaoxingLogin/src/main/java/com/xiaoxing/login/mvp/ui/activity/RegisterVerificationCodeActivity.java
@@ -4,19 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
 import com.xiaoxing.login.R;
+import com.xiaoxing.login.R2;
 import com.xiaoxing.login.di.component.DaggerRegisterVerificationCodeComponent;
 import com.xiaoxing.login.di.module.RegisterVerificationCodeModule;
 import com.xiaoxing.login.mvp.contract.RegisterVerificationCodeContract;
 import com.xiaoxing.login.mvp.presenter.RegisterVerificationCodePresenter;
 
+import butterknife.OnClick;
 import me.jessyan.armscomponent.commonres.utils.ToolbarUtils;
 import me.jessyan.armscomponent.commonsdk.core.RouterHub;
+import me.jessyan.armscomponent.commonsdk.utils.Utils;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
@@ -40,7 +44,7 @@ public class RegisterVerificationCodeActivity extends BaseActivity<RegisterVerif
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
-        ToolbarUtils.initToolbarTitleNoBack(this, getString(R.string.xiaoxing_login_verification));
+        ToolbarUtils.initToolbarTitleBack(this, getString(R.string.xiaoxing_login_verification));
 
     }
 
@@ -69,5 +73,13 @@ public class RegisterVerificationCodeActivity extends BaseActivity<RegisterVerif
     @Override
     public void killMyself() {
         finish();
+    }
+
+    @OnClick({R2.id.btn_next})
+    public void onViewClicked(View view) {
+        if (view.getId() == R.id.btn_next) {
+            Utils.navigation(RegisterVerificationCodeActivity.this, RouterHub.XIAO_XING_LOGIN_REGISTERVERIFICATIONCODEACTIVITY);
+        }
+
     }
 }
