846f8818a0e3a6f69916f7a4010cb27179594402
xiaoxing <753940262@qq.com>
2018-06-16 11:59:57 +0800

    1、专场筛选抽屉添加

--- SalesClient/src/main/java/com/xiaoxing/salesclient/mvp/ui/activity/ZhuanChangActivity.java
+++ SalesClient/src/main/java/com/xiaoxing/salesclient/mvp/ui/activity/ZhuanChangActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.jess.arms.base.BaseActivity;
@@ -22,23 +28,34 @@ import com.xiaoxing.salesclient.mvp.ui.fragment.FragmentReMenPaiMai;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import me.jessyan.armscomponent.commonres.utils.SlidingTabLayoutUtil;
 import me.jessyan.armscomponent.commonres.utils.ToolbarUtils;
 import me.jessyan.armscomponent.commonsdk.core.RouterHub;
 import me.jessyan.armscomponent.commonsdk.utils.Utils;
 import xiaoxing.com.salesclient.R;
+import xiaoxing.com.salesclient.R2;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
 @Route(path = RouterHub.SALES_CLIENT_ZHUANCHANGACTIVITY)
-public class ZhuanChangActivity extends BaseActivity<ZhuanChangPresenter> implements ZhuanChangContract.View {
+public class ZhuanChangActivity extends BaseActivity<ZhuanChangPresenter> implements ZhuanChangContract.View, NavigationView.OnNavigationItemSelectedListener {
 
 
     private final String[] mTitles = {"所有专场", "热门拍品", "我的关注", "筛选"};
 
+    @BindView(R2.id.tv_shai_xuan)
+    TextView mTvShaiXuan;
+    @BindView(R2.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R2.id.nav_view)
+    NavigationView navigationView;
+
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
+
         DaggerZhuanChangComponent //如找不到该类,请编译一下项目
                 .builder()
                 .appComponent(appComponent)
@@ -70,6 +87,14 @@ public class ZhuanChangActivity extends BaseActivity<ZhuanChangPresenter> implem
         mFragments.add(new FragmentReMenPaiMai());
 
         SlidingTabLayoutUtil.init(this, mTitles, mFragments);
+
+
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -97,5 +122,46 @@ public class ZhuanChangActivity extends BaseActivity<ZhuanChangPresenter> implem
     @Override
     public void killMyself() {
         finish();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @OnClick(R2.id.tv_shai_xuan)
+    void shaiXuan() {
+        drawer.openDrawer(GravityCompat.START);
     }
 }
