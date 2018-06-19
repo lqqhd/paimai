d04614a4c4c333ef9bd521f003a88921d93ccb76
xiaoxing <753940262@qq.com>
2018-06-19 18:03:05 +0800

    1、发布跳转链接

--- SalesClient/src/main/java/com/xiaoxing/salesclient/mvp/ui/fragment/FragmentRelease.java
+++ SalesClient/src/main/java/com/xiaoxing/salesclient/mvp/ui/fragment/FragmentRelease.java
@@ -10,11 +10,19 @@ import android.view.ViewGroup;
 import com.jess.arms.base.BaseFragment;
 import com.jess.arms.di.component.AppComponent;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import me.jessyan.armscomponent.commonres.utils.ToolbarUtils;
+import me.jessyan.armscomponent.commonsdk.core.RouterHub;
+import me.jessyan.armscomponent.commonsdk.utils.Utils;
 import xiaoxing.com.salesclient.R;
+import xiaoxing.com.salesclient.R2;
 
 public class FragmentRelease extends BaseFragment {
 
+
+    Unbinder unbinder;
 
     public static FragmentRelease newInstance(String content) {
         Bundle args = new Bundle();
@@ -47,5 +55,31 @@ public class FragmentRelease extends BaseFragment {
     @Override
     public void setData(@Nullable Object data) {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R2.id.tv_wei_pai_cang_pin_fa_bu, R2.id.tv_zhuan_chang_huo_dong_fa_huo, R2.id.tv_zhan_ting_cang_pin_fa_bu})
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.tv_wei_pai_cang_pin_fa_bu) {
+            Utils.navigation(getActivity(), RouterHub.SALES_CLIENT_CHUANGJIANWEIPAIACTIVITY);
+        } else if (i == R.id.tv_zhuan_chang_huo_dong_fa_huo) {
+            Utils.navigation(getActivity(), RouterHub.SALES_CLIENT_CHUANGJIANZHUANCHANGACTIVITY);
+        } else if (i == R.id.tv_zhan_ting_cang_pin_fa_bu) {
+            Utils.navigation(getActivity(), RouterHub.SALES_CLIENT_CHUANGJIANZHANPINACTIVITY);
+        }
     }
 }
