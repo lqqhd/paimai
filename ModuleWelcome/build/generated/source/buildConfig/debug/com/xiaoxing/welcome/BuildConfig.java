f8709bcbd39c0ee2b55acf535f4b371ea87f95a7
xiaoxing <753940262@qq.com>
2018-07-20 20:57:36 +0800

    1、备份

---
+++ ModuleWelcome/build/generated/source/buildConfig/debug/com/xiaoxing/welcome/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.xiaoxing.welcome;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.xiaoxing.welcome";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Fields from build type: debug
+  public static final boolean IS_BUILD_MODULE = false;
+  public static final boolean IS_SALALS_CLIENT = false;
+  public static final boolean LOG_DEBUG = true;
+  public static final boolean USE_CANARY = true;
+}
