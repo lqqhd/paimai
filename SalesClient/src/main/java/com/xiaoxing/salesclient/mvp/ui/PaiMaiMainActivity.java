51f3b098113cfc6d2c6a569ad7cc0abb83682be7
xiaoxing <753940262@qq.com>
2018-08-09 02:11:23 +0800

    1、PaiMaiMainActivity 添加onDestroy 方法，销毁时把对象释放

--- SalesClient/src/main/java/com/xiaoxing/salesclient/mvp/ui/PaiMaiMainActivity.java
+++ SalesClient/src/main/java/com/xiaoxing/salesclient/mvp/ui/PaiMaiMainActivity.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -34,9 +33,6 @@ import com.xiaoxing.salesclient.di.component.DaggerPaiMaiMainComponent;
 import com.xiaoxing.salesclient.di.module.PaiMaiMainModule;
 import com.xiaoxing.salesclient.mvp.contract.PaiMaiMainContract;
 import com.xiaoxing.salesclient.mvp.presenter.PaiMaiMainPresenter;
-
-import me.jessyan.armscomponent.commonres.utils.AppUpdate;
-
 import com.xiaoxing.salesclient.mvp.ui.popup.QianDaoPopup;
 import com.xiaoxing.salesclient.mvp.ui.viewpager.MyViewPagerAdapter;
 
@@ -44,6 +40,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import me.jessyan.armscomponent.commonres.utils.AppUpdate;
 import me.jessyan.armscomponent.commonres.utils.CheckVersionUtil;
 import me.jessyan.armscomponent.commonres.utils.CouponsReceive;
 import me.jessyan.armscomponent.commonres.utils.MySgninCouponsList;
@@ -389,6 +386,13 @@ public class PaiMaiMainActivity extends BaseActivity<PaiMaiMainPresenter> implem
             return false;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (instance != null)
+            instance = null;
     }
 
 }
